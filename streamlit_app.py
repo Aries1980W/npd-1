2f6c9983b9d4cacda55fd43a76cf0892941ae006
AriesW1980 <132648390+AriesW1980@users.noreply.github.com>
2023-05-06 23:17:21 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -293,8 +293,8 @@ st.write('-----------------------------------')
 
 col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12 = st.columns(12, gap="small")
 with col1:
-    st.write(str(pdt_code)+'_'+'1'+'.jpg')        
-    st.image(Image.open(str(pdt_code[0])+'_'+'1'+'.jpg')) #,width=320 ,use_column_width=True
+    st.write(str(pdt_code[0])+'_1.jpg')        
+    st.image(Image.open(str(pdt_code[0])+_1.jpg)) #,width=320 ,use_column_width=True
 with col2:
     st.image(Image.open(str(pdt_code[0])+'_'+'2'+'.jpg'))
 with col3:
